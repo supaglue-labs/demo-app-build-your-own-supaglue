be203594b5ec45cf0d06ccb2f321bd2d79d4b87c
Tony Xiao <tonyx.ca@gmail.com>
2024-01-12 14:16:12 -0800

    chore: Initial commit with monorepo structure

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+/** @type {import('@commitlint/types').UserConfig} */
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0],
+  },
+}
---
+++ jest.config.js
@@ -0,0 +1,38 @@
+import hq from 'alias-hq'
+
+/**
+ * @type {import('jest').Config}
+ */
+export default {
+  extensionsToTreatAsEsm: ['.ts'],
+  testEnvironment: 'node',
+  resolver: '<rootDir>/jest.resolver.cjs',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+  ],
+  watchPathIgnorePatterns: [
+    '\\.gen\\.d\\.ts',
+    '\\.gen\\.ts',
+    '\\.gen\\.json',
+    '\\.schema\\.json',
+  ],
+  testRegex: '\\.(spec|test)\\.[jt]sx?$',
+  transform: {
+    '^.+\\.(js|ts|tsx)$': [
+      'esbuild-jest',
+      {
+        sourcemap: true,
+        target: 'node20',
+        format: 'esm',
+      },
+    ],
+  },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  // https://github.com/davestewart/alias-hq/blob/main/docs/api/api.md
+  // maybe this would be a reason to use ts-jest which has native support for pathsToModuleNameMapper
+  // instead of esbuild-jest which does not?
+  moduleNameMapper: hq.get('jest'),
+}
