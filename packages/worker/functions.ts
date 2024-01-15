86c3292c033c24d8d73e9ebc3ac64b7e160a7c25
Tony Xiao <tonyx.ca@gmail.com>
2024-01-15 10:16:04 -0800

    feat: Use step function for sync

--- packages/worker/functions.ts
+++ packages/worker/functions.ts
@@ -38,7 +38,7 @@ export const scheduleSyncs = inngest.createFunction(
 export const syncConnection = inngest.createFunction(
   {id: 'sync-connection'},
   {event: 'connection/sync'},
-  async ({event}) => {
+  async ({event, step}) => {
     const {
       data: {connectionId, providerConfigKey},
     } = event
@@ -56,36 +56,37 @@ export const syncConnection = inngest.createFunction(
       },
     })
 
+    // TODO: Need to figure out if stepFunction behaves as expected...
     let cursor = null as null | string | undefined
-    while (true) {
-      const res = await supaglue.GET('/engagement/v2/sequences', {
-        params: {query: {cursor}},
-      })
-      console.log('Syncing sequences count=', res.data.items.length)
-      await db
-        .insert(engagementSequences)
-        .values(
-          res.data.items.map((item) => ({
-            supaglueApplicationId: '$YOUR_APPLICATION_ID',
-            supaglueCustomerId: connectionId, //  '$YOUR_CUSTOMER_ID',
-            supaglueProviderName: providerConfigKey,
-            id: item.id,
-            lastModifiedAt: new Date().toISOString(),
-            supaglueEmittedAt: new Date().toISOString(),
-            isDeleted: false,
-            // TODO: Return both raw and unified data here...
-            // Workaround jsonb support issue... https://github.com/drizzle-team/drizzle-orm/issues/724
-            rawData: sql`${item}::jsonb`,
-            supaglueUnifiedData: sql`${item}::jsonb`,
-          })),
-        )
-        .onConflictDoNothing()
+    do {
+      cursor = await step.run(`sync-page-${cursor}`, async () => {
+        const res = await supaglue.GET('/engagement/v2/sequences', {
+          params: {query: {cursor}},
+        })
+        console.log('Syncing sequences count=', res.data.items.length)
+        await db
+          .insert(engagementSequences)
+          .values(
+            res.data.items.map((item) => ({
+              supaglueApplicationId: '$YOUR_APPLICATION_ID',
+              supaglueCustomerId: connectionId, //  '$YOUR_CUSTOMER_ID',
+              supaglueProviderName: providerConfigKey,
+              id: item.id,
+              lastModifiedAt: new Date().toISOString(),
+              supaglueEmittedAt: new Date().toISOString(),
+              isDeleted: false,
+              // TODO: Return both raw and unified data here...
+              // Workaround jsonb support issue... https://github.com/drizzle-team/drizzle-orm/issues/724
+              rawData: sql`${item}::jsonb`,
+              supaglueUnifiedData: sql`${item}::jsonb`,
+            })),
+          )
+          .onConflictDoNothing()
 
-      if (!res.data.nextPageCursor) {
-        break
-      }
-      cursor = res.data.nextPageCursor
-    }
+        return res.data.nextPageCursor
+      })
+    } while (cursor)
+
     console.log('[syncConnection] Complete', {
       connectionId,
       providerConfigKey,
