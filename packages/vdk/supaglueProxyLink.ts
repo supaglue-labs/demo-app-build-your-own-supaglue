38c706f52c0c4c7ea833719b9fc8488ab9e68dd6
Tony Xiao <tonyx.ca@gmail.com>
2024-02-06 21:19:34 -0800

    feat: Impl supaglue proxy link to allow incremental migration

---
+++ packages/vdk/supaglueProxyLink.ts
@@ -0,0 +1,66 @@
+import type {Link as FetchLink} from '@opensdks/fetch-links'
+import {mergeHeaders, modifyRequest} from '@opensdks/fetch-links'
+
+interface SupaglueHeaders {
+  'x-api-key': string
+  'x-customer-id': string
+  'x-provider-name': string
+}
+
+/** https://docs.supaglue.com/api/v2/actions/send-passthrough-request */
+export function supaglueProxyLink(opts: {
+  apiKey: string
+  /** `x-customer-id` header */
+  customerId: string
+  /** `x-provider-name` header */
+  providerName: string
+}): FetchLink {
+  const supaglueHeaders = {
+    'x-api-key': opts.apiKey,
+    'x-customer-id': opts.customerId,
+    'x-provider-name': opts.providerName,
+  } satisfies SupaglueHeaders
+
+  return async (req, next) => {
+    const url = new URL(req.url)
+    // Can be JSON object or text
+    const body = await req.text()
+
+    const res = await next(
+      modifyRequest(req, {
+        url: 'https://api.supaglue.io/actions/v2/passthrough',
+        headers: mergeHeaders(req.headers, supaglueHeaders),
+        method: 'POST',
+        body: JSON.stringify({
+          path: url.pathname,
+          method: req.method,
+          headers: Object.fromEntries(req.headers.entries()),
+          query: Object.fromEntries(url.searchParams.entries()),
+          // Sending body for get / head requests results in failure
+          ...(body && {body}),
+        }),
+      }),
+    )
+    if (res.status !== 200) {
+      return res
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const resJson: {
+      url: string
+      status: number
+      headers: Record<string, string>
+      body: string | object
+    } = await res.json()
+    return new Response(
+      typeof resJson.body === 'string'
+        ? resJson.body
+        : JSON.stringify(resJson.body),
+      {
+        headers: resJson.headers,
+        status: resJson.status,
+        statusText: `From ${resJson.url}`,
+      },
+    )
+  }
+}
