9c183a5d44eb7d30ffab3aa8bbc09125ef029ab0
Tony Xiao <tonyx.ca@gmail.com>
2024-02-19 04:03:04 -0800

    fix: comment out code not yet protected for now

--- main/app/page.tsx
+++ main/app/page.tsx
@@ -5,25 +5,25 @@ import {env} from '@/env'
 import {NewConnection} from './NewConnection'
 
 async function listConnections() {
-  if (env.SUPAGLUE_API_KEY && featureFlags.mode === 'supaglue') {
-    const supaglue = initSupaglueSDK({
-      headers: {'x-api-key': env.SUPAGLUE_API_KEY},
-    })
-    const cusRes = await supaglue.mgmt.GET('/customers')
-    const connections = await Promise.all(
-      cusRes.data.map((c) =>
-        supaglue.mgmt.GET('/customers/{customer_id}/connections', {
-          params: {path: {customer_id: c.customer_id}},
-        }),
-      ),
-    ).then((resArray) => resArray.flatMap((res) => res.data))
-    return connections.map((c) => ({
-      id: c.id,
-      connection_id: `customerId: ${c.customer_id}\n connectionId: ${c.id}`,
-      provider: c.provider_name,
-      // customer_id: c.customer_id,
-    }))
-  }
+  // if (env.SUPAGLUE_API_KEY && featureFlags.mode === 'supaglue') {
+  //   const supaglue = initSupaglueSDK({
+  //     headers: {'x-api-key': env.SUPAGLUE_API_KEY},
+  //   })
+  //   const cusRes = await supaglue.mgmt.GET('/customers')
+  //   const connections = await Promise.all(
+  //     cusRes.data.map((c) =>
+  //       supaglue.mgmt.GET('/customers/{customer_id}/connections', {
+  //         params: {path: {customer_id: c.customer_id}},
+  //       }),
+  //     ),
+  //   ).then((resArray) => resArray.flatMap((res) => res.data))
+  //   return connections.map((c) => ({
+  //     id: c.id,
+  //     connection_id: `customerId: ${c.customer_id}\n connectionId: ${c.id}`,
+  //     provider: c.provider_name,
+  //     // customer_id: c.customer_id,
+  //   }))
+  // }
   if (env.NANGO_SECRET_KEY && featureFlags.mode === 'nango') {
     const nango = initNangoSDK({
       headers: {authorization: `Bearer ${env.NANGO_SECRET_KEY}`},
