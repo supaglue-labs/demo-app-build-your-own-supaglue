54e632aef13928e1ec0a8bee099342e095fabe6f
Tony Xiao <tonyx.ca@gmail.com>
2024-02-22 02:09:23 -0800

    feat: extract common and make it work for companies

--- verticals/vertical-crm/providers/hubspot-provider.ts
+++ verticals/vertical-crm/providers/hubspot-provider.ts
@@ -1,3 +1,4 @@
+import type {BaseRecord} from '@supaglue/vdk'
 import {LastUpdatedAtNextOffset, mapper, z} from '@supaglue/vdk'
 import type {Oas_crm_contacts} from '@opensdks/sdk-hubspot'
 import {initHubspotSDK, type HubspotSDK} from '@opensdks/sdk-hubspot'
@@ -23,7 +24,19 @@ export const HUBSPOT_STANDARD_OBJECTS = [
   'postal_mail',
   'task',
 ] as const
-
+const HSBase = z.object({
+  id: z.string(),
+  properties: z
+    .object({
+      hs_object_id: z.string(),
+      createdate: z.string().optional(),
+      lastmodifieddate: z.string().optional(),
+    })
+    .passthrough(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+  archived: z.boolean(),
+})
 const HSContact = z.object({
   id: z.string(),
   properties: z.object({
@@ -41,12 +54,108 @@ const HSContact = z.object({
 })
 
 const mappers = {
+  account: mapper(HSBase, commonModels.account, {
+    id: 'id',
+    updated_at: 'updatedAt',
+  }),
   contact: mapper(HSContact, commonModels.contact, {
     id: 'id',
     first_name: 'properties.firstname',
     last_name: 'properties.lastname',
     updated_at: 'updatedAt',
   }),
+  opportunity: mapper(HSBase, commonModels.opportunity, {
+    id: 'id',
+    name: 'id',
+    updated_at: 'updatedAt',
+  }),
+  lead: mapper(HSBase, commonModels.lead, {
+    id: 'id',
+    updated_at: 'updatedAt',
+  }),
+  user: mapper(HSBase, commonModels.user, {
+    id: 'id',
+    updated_at: 'updatedAt',
+  }),
+}
+const _listEntityThenMap = async <TIn, TOut extends BaseRecord>(
+  instance: HubspotSDK,
+  {
+    entity,
+    fields,
+    ...opts
+  }: {
+    entity: string
+    fields: Array<Extract<keyof TIn, string>>
+    mapper: {parse: (rawData: unknown) => TOut; _in: TIn}
+    page_size?: number
+    cursor?: string | null
+  },
+) => {
+  const limit = opts?.page_size ?? 100
+  const cursor = LastUpdatedAtNextOffset.fromCursor(opts?.cursor)
+  const kLastModifiedAt = entity === 'contacts' ?  'lastmodifieddate' : 'hs_lastmodifieddate'
+  // We may want to consider using the list rather than search endpoint for this stuff...
+  const res = await instance[`crm_${entity as 'contacts'}`].POST(
+    `/crm/v3/objects/${entity as 'contacts'}/search`,
+    {
+      body: {
+        properties: [
+          'hs_object_id',
+          'createdate',
+          'lastmodifieddate',
+          'hs_lastmodifieddate',
+          ...fields,
+        ],
+        filterGroups: cursor?.last_updated_at
+          ? [
+              {
+                filters: [
+                  {
+                    propertyName: kLastModifiedAt,
+                    operator: 'GTE',
+                    value: cursor?.last_updated_at,
+                  },
+                ],
+              },
+            ]
+          : [],
+        after: cursor?.next_offset ?? '',
+        sorts: [
+          {
+            propertyName: kLastModifiedAt,
+            direction: 'ASCENDING',
+          },
+          // Cannot sort by multiple values unfortunately...
+          // {
+          //   propertyName: 'hs_object_id',
+          //   direction: 'ASCENDING',
+          // },
+        ] as unknown as string[],
+        limit,
+      },
+    },
+  )
+  const items = res.data.results.map(opts.mapper.parse)
+  const lastItem = items[items.length - 1]
+  return {
+    items,
+    // Not the same as simply items.length === 0
+    has_next_page: !!res.data.paging?.next?.after,
+    next_cursor:
+      (lastItem
+        ? LastUpdatedAtNextOffset.toCursor({
+            last_updated_at: lastItem.updated_at,
+            next_offset:
+              // offset / offset-like cursor is only usable if the filtering criteria doesn't change, notably the last_updated_at timestamp
+              // in practice this means that we only care about `after` offset when we have more than `limit` number of items modified at the exact
+              // same timestamp
+              lastItem.updated_at === cursor?.last_updated_at
+                ? res.data.paging?.next?.after
+                : undefined,
+          })
+        : opts?.cursor) ?? null,
+  }
 }
 
 export const hubspotProvider = {
@@ -55,71 +164,41 @@ export const hubspotProvider = {
       headers: {authorization: 'Bearer ...'}, // This will be populated by Nango, or you can populate your own...
       links: (defaultLinks) => [...proxyLinks, ...defaultLinks],
     }),
-  listContacts: async ({instance, input: opts}) => {
-    const limit = opts?.page_size ?? 100
-    const cursor = LastUpdatedAtNextOffset.fromCursor(opts?.cursor)
-    const kLastModifiedAt = 'lastmodifieddate'
-    // We may want to consider using the list rather than search endpoint for this stuff...
-    const res = await instance.crm_contacts.POST(
-      '/crm/v3/objects/contacts/search',
-      {
-        body: {
-          properties: [
-            'hs_object_id',
-            'hs_lastmodifieddate',
-            'createdate',
-            'lastmodifieddate',
-          ],
-          filterGroups: cursor?.last_updated_at
-            ? [
-                {
-                  filters: [
-                    {
-                      propertyName: kLastModifiedAt,
-                      operator: 'GTE',
-                      value: cursor?.last_updated_at,
-                    },
-                  ],
-                },
-              ]
-            : [],
-          after: cursor?.next_offset ?? '',
-          sorts: [
-            {
-              propertyName: 'lastmodifieddate',
-              direction: 'ASCENDING',
-            },
-            // Cannot sort by multiple values unfortunately...
-            // {
-            //   propertyName: 'hs_object_id',
-            //   direction: 'ASCENDING',
-            // },
-          ] as unknown as string[],
-          limit,
-        },
-      },
-    )
-    const items = res.data.results.map(mappers.contact.parse)
-    const lastItem = items[items.length - 1]
-    return {
-      items: res.data.results.map(mappers.contact.parse),
-      // Not the same as simply items.length === 0
-      has_next_page: !!res.data.paging?.next?.after,
-      next_cursor:
-        (lastItem
-          ? LastUpdatedAtNextOffset.toCursor({
-              last_updated_at: lastItem.updated_at,
-              next_offset:
-                // offset / offset-like cursor is only usable if the filtering criteria doesn't change, notably the last_updated_at timestamp
-                // in practice this means that we only care about `after` offset when we have more than `limit` number of items modified at the exact
-                // same timestamp
-                lastItem.updated_at === cursor?.last_updated_at
-                  ? res.data.paging?.next?.after
-                  : undefined,
-            })
-          : opts?.cursor) ?? null,
-    }
-  },
+  listContacts: async ({instance, input}) =>
+    _listEntityThenMap(instance, {
+      ...input,
+      entity: 'contacts',
+      mapper: mappers.contact,
+      fields: [],
+    }),
+  listAccounts: async ({instance, input}) =>
+    _listEntityThenMap(instance, {
+      ...input,
+      entity: 'companies',
+      mapper: mappers.account,
+      fields: [],
+    }),
+  listOpportunities: async ({instance, input}) =>
+    _listEntityThenMap(instance, {
+      ...input,
+      entity: 'opportunities',
+      mapper: mappers.opportunity,
+      fields: [],
+    }),
+  listLeads: async ({instance, input}) =>
+    _listEntityThenMap(instance, {
+      ...input,
+      entity: 'leads',
+      mapper: mappers.lead,
+      fields: [],
+    }),
+  listUsers: async ({instance, input}) =>
+    _listEntityThenMap(instance, {
+      ...input,
+      entity: 'users',
+      mapper: mappers.user,
+      fields: [],
+    }),
   // eslint-disable-next-line @typescript-eslint/require-await
   getAccount: async ({}) => {
     throw new Error('Not implemented yet')
